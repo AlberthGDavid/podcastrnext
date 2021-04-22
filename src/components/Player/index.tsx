0a3c7570f6f6a4078dfd30b1fb6bdb07e3e42b5b
Alberth David <alberth.david@b2egroup.com.br>
2021-04-22 20:25:21 -0300

    commit

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,39 +1,103 @@
+import Image from 'next/image';
 
+import { useContext, useEffect, useRef } from 'react';
+import { PlayerContext } from '../../contexts/PlayerContexts';
 import styles from './styles.module.scss';
+import Slider from 'rc-slider';
+
+import 'rc-slider/assets/index.css';
+
 export function Player() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  const { episodeList,
+    currentEpisodeIndex,
+    isPlaying,
+    tooglePlay,
+    setIsPlayingState
+  } = useContext(PlayerContext);
+  const episode = episodeList[currentEpisodeIndex];
+
+  useEffect(() => {
+    if (!audioRef.current) {
+      return;
+    }
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying])
+
   return (
     <div className={styles.playerContainer}>
       <header>
         <img src="/playing.svg" alt="Tocando Agora" />
-        <strong>Tocando Agora</strong>
+        <strong>Tocando Agora {episode?.title}</strong>
       </header>
-      <div className={styles.emptyPlayer}>
-        <strong>Selecione um posdcast para ouvir</strong>
-      </div>
 
-      <footer className={styles.empty}>
+      { episode ? (
+        <div className={styles.currentEpisode}>
+          <Image
+            width={592}
+            height={592}
+            src={episode.thumbnail}
+            objectFit="cover"
+          />
+          <strong>{episode.title}</strong>
+          <strong>{episode.members}</strong>
+        </div>
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um posdcast para ouvir</strong>
+        </div>
+      )}
+
+
+      <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: '#04d361' }}
+                railStyle={{ backgroundColor: '#9f75ff' }}
+                handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )}
           </div>
           <span>00:00</span>
         </div>
 
+        {episode && (
+          <audio
+            src={episode.url}
+            ref={audioRef}
+            autoPlay
+            onPlay={() => setIsPlayingState(true)}
+            onPause={() => setIsPlayingState(false)}
+          />
+        )}
+
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Embaralhar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Tocar Anterior" />
           </button>
-          <button type="button" className={styles.playButton}>
-            <img src="/play.svg" alt="Tocar" />
+          <button type="button" className={styles.playButton} disabled={!episode}
+            onClick={tooglePlay}>
+            {isPlaying
+              ? <img src="/pause.svg" alt="Tocar" />
+              : <img src="/play.svg" alt="Tocar" />}
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Tocar proxima" />
-          </button>
-          <button type="button">
+          </button>,
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" alt="Repetir" />
           </button>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ import ptBR from "date-fns/locale/pt-BR";
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
 import styles from "./home.module.scss";
+import { useContext } from "react";
+import { PlayerContext } from "../contexts/PlayerContexts";
 
 
 type Episode = {
@@ -26,6 +28,8 @@ type HomeProps = {
 };
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -51,7 +55,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
